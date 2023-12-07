d19c8210446cefdf3a552aeec0671574a2583bc2
sgomes1000 <sabrina.gomes.ca@gmail.com>
2023-12-07 10:09:49 -0700

    first upload

---
+++ aivision.py
@@ -0,0 +1,53 @@
+import os
+import azure.ai.vision as sdk
+
+service_options = sdk.VisionServiceOptions(os.environ["VISION_ENDPOINT"],
+                                           os.environ["VISION_KEY"])
+
+file_path = ('image.jpg')
+
+vision_source = sdk.VisionSource(file_path)
+
+analysis_options = sdk.ImageAnalysisOptions()
+
+analysis_options.features = (
+    sdk.ImageAnalysisFeature.CAPTION |
+    sdk.ImageAnalysisFeature.TEXT
+)
+
+analysis_options.language = "en"
+
+analysis_options.gender_neutral_caption = True
+
+image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+
+result = image_analyzer.analyze()
+
+if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+    if result.caption is not None:
+        print(" Caption:")
+        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
+
+    if result.text is not None:
+        print(" Text:")
+        words_list = []  # Initialize an empty list to store words
+        for line in result.text.lines:
+            points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
+            print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
+            for word in line.words:
+                words_list.append(word.content)  # Add each word to the list
+                points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
+                print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
+                      .format(word.content, points_string, word.confidence))
+                    # Print the list of words
+                print("List of Words:", words_list)
+                
+
+else:
+
+    error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+    print(" Analysis failed.")
+    print("   Error reason: {}".format(error_details.reason))
+    print("   Error code: {}".format(error_details.error_code))
+    print("   Error message: {}".format(error_details.message))
